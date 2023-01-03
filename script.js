2ed33b41022635c3f8a50bd5c8f88e08e196ca8f
ALEXANDREGBAMELE <112797022+ALEXANDREGBAMELE@users.noreply.github.com>
2023-01-03 08:08:08 +0100

    update version 2

---
+++ script.js
@@ -0,0 +1,32 @@
+
+        let wordSeparetedByComa = prompt("Saisissez des mots séparé par des virgule svp :");
+        let wordTab = wordSeparetedByComa.split(",");
+        let maxLenght = -1;
+        let asterix = '';
+        let space = ' ';
+        for(let i of wordTab) {
+            if(i.length > maxLenght) {
+                maxLenght = i.length;
+            }
+        }
+        
+        maxLenght+=4;
+        
+        for(let i = 1; i <= maxLenght;i++) {
+            asterix+='*';
+        }
+        console.log(asterix);
+        for(let i of wordTab) {
+            if(i.length < (maxLenght -4)) {
+                //space*=(maxLenght-4-Number(i.length));
+                for(let j=0; j<(maxLenght-4-Number(i.length));j++) {
+                    space+=' ';
+                }
+                console.log(`* ${i}${space}*`);
+                space = ' ';
+            }else {
+                console.log(`* ${i} *`);
+            }
+        
+        }
+        console.log(asterix);
